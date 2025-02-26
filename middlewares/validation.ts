9962fb91f4d85ba613ae7c340e6162e5cdcd889d
ilyavotos <ilyavotos@gmail.com>
2025-02-26 11:51:42 +0300

    fix: validation error

--- middlewares/validation.ts
+++ middlewares/validation.ts
@@ -13,7 +13,7 @@ export const validateData = (schema: z.ZodObject<any, any>) => {
         }));
         res.status(400).json({ error: "Invalid data", details: errorMessages });
       } else {
-        res.status(500).json({ error: "Internal Server Error" });
+        next(error);
       }
     }
   };
